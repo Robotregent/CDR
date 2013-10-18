936bb00fc5a480dd23d94abcfe5bb27f83de3091
Fabian Schneider <fabian.schneider@bit-care.de>
2013-10-18 15:59:10 +0200

    Fixing docu

---
+++ cdr-main/service-api/src/main/java/example/TodoItem.java
@@ -0,0 +1,23 @@
+package example;
+public class TodoItem {	
+	
+	private String description = "To Do";
+	private int prio = 5;
+	
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String descrition) {
+		this.description = descrition;
+	}
+
+	public int getPrio() {
+		return prio;
+	}
+	public void setPrio(int prio) {
+		this.prio = prio;
+	}
+}
+
